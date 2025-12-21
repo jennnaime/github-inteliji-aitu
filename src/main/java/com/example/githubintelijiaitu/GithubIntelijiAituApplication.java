7720d47ab8cbc5214419e047af6ac5e280fbf7c6
jennnaime <aishazhussupova@gmail.com>
2025-12-21 11:31:20 +0500

    init commit

---
+++ src/main/java/com/example/githubintelijiaitu/GithubIntelijiAituApplication.java
@@ -0,0 +1,13 @@
+package com.example.githubintelijiaitu;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GithubIntelijiAituApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GithubIntelijiAituApplication.class, args);
+	}
+
+}
